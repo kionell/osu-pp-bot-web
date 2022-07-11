b6fd4fdbfbe890f534c9500d0283729d7662296b
kionell <sashakostyunin1@gmail.com>
2022-07-11 12:52:11 +0300

    Disable auto indexing for performance model

--- src/performance/models/performance.model.ts
+++ src/performance/models/performance.model.ts
@@ -1,7 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
-@Schema({ autoCreate: false })
+@Schema({ autoCreate: false, _id: false })
 export class Performance {
   @Prop({ required: true })
     mods: string;
