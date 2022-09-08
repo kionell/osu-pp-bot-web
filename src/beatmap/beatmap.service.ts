62928f2eed49fa30b5d32f64a222af64470e98f7
kionell <sashakostyunin1@gmail.com>
2022-09-08 09:57:16 +0300

    Don't check for strain graph file before sending

--- src/beatmap/beatmap.service.ts
+++ src/beatmap/beatmap.service.ts
@@ -1,4 +1,3 @@
-import fs from 'fs';
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { IBeatmapResponse } from './interfaces/beatmap-response.interface';
 import { BeatmapOptionsDto } from './dto/beatmap-options.dto';
@@ -62,18 +61,20 @@ export class BeatmapService {
 
     if (!cached) return await this.createAndSaveBeatmap(options);
 
-    const strainGraphPath = process.env.STRAINS_PATH + `/${cached.graphFile}`;
+    return cached ?? await this.createAndSaveBeatmap(options);
+
+    // const strainGraphPath = process.env.STRAINS_PATH + `/${cached.graphFile}`;
 
     /**
      * Recalculate beatmap if strain graph is not found.
      * This is a lazy fix for the ephemeral storage problem when all 
      * strain graphs are constantly getting cleared after some short time.
      */
-    return await new Promise((res) => {
-      fs.promises.access(strainGraphPath, fs.constants.F_OK)
-        .then(() => res(cached))
-        .catch(async () => res(await this.createAndSaveBeatmap(options)));
-    });
+    // return await new Promise((res) => {
+    //   fs.promises.access(strainGraphPath, fs.constants.F_OK)
+    //     .then(() => res(cached))
+    //     .catch(async () => res(await this.createAndSaveBeatmap(options)));
+    // });
   }
 
   /**
