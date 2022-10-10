86e869720d19323347b8dd70a7683eaa197c6d96
Alexander <sashakostyunin1@gmail.com>
2022-10-10 13:23:16 +0300

    Add count300 to the score options DTO

--- src/score/dto/score-options.dto.ts
+++ src/score/dto/score-options.dto.ts
@@ -90,6 +90,11 @@ export class ScoreOptionsDto {
   count100?: number;
 
   /**
+   * Target score 300's.
+   */
+  count300?: number;
+
+  /**
    * Target score katu hits.
    */
   countKatu?: number;
