f5a767a219d0ca13bb35c31828b9fbe2af7a7025
Alexander <sashakostyunin1@gmail.com>
2022-10-10 13:11:50 +0300

    Add geki and katu counts to the score options DTO

--- src/score/dto/score-options.dto.ts
+++ src/score/dto/score-options.dto.ts
@@ -90,6 +90,16 @@ export class ScoreOptionsDto {
   count100?: number;
 
   /**
+   * Target score katu hits.
+   */
+  countKatu?: number;
+
+  /**
+   * Target score geki hits.
+   */
+  countGeki?: number;
+
+  /**
    * Target score accuracy.
    */
   accuracy?: number;
